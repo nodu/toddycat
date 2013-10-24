521a67ccc1c50ca030c505824f0ba08e053b64d0
chasm <charles.munat@gmail.com>
2013-10-24 12:16:16 +0800

    Completed authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User
   
   before_save :encrypt_password
   
+  def authenticate(password)
+    self.fish == BCrypt::Engine.hash_secret(password, self.salt)
+  end
+  
   private
   
   def encrypt_password
