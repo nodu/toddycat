06e36a37eaaf5b6e986592a2643c0bcb2963919a
chasm <charles.munat@gmail.com>
2013-10-24 15:48:57 +0800

    Finished basic auth, registration, reset except mailers.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,13 +6,41 @@ class SessionController < ApplicationController
   end
   
   def create
-    @user = User.find_by(email: params[:user][:email])
-    
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to root_url
+    # is the password blank?
+    if params[:user][:password].blank?
+      if @user = User.find_by(email: params[:user][:email])
+        @user.code = SecureRandom.urlsafe_base64
+        @user.expires_at = Time.now + 1.day
+        @user.save
+        
+        # SEND PASSWORD RESET EMAIL
+        
+        flash.now[:notice] = "An email with instructions for " +
+          "reseting your password has been sent to you."
+        render :new
+      else
+        @registrant = Registrant.new
+        @registrant.id = SecureRandom.urlsafe_base64
+        @registrant.email = params[:user][:email]
+        @registrant.expires_at = Time.now + 1.day
+        @registrant.save
+        
+        # SEND REGISTRATION EMAIL
+        
+        flash.now[:notice] = "An email with instructions for " +
+          "completing your registration has been sent to you."
+        render :new
+      end
     else
-      render :new, error: "Unable to sign you in. Please try again."
+      # attempt to authenticate
+      @user = User.find_by(email: params[:user][:email])
+  
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to root_url
+      else
+        render :new, error: "Unable to sign you in. Please try again."
+      end
     end
   end
   
