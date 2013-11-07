e3eac6982d21e3db1f98ac1e58070c76043576e7
Matt Nodurfth <mnodurft@gmail.com>
2013-11-07 16:25:40 +0800

    Added comment about saving black nested fields

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -4,6 +4,8 @@ class Floor
   has_many :flats
   accepts_nested_attributes_for :flats, :allow_destroy => true
   #when save floor, please save the flats too :D
+  
+  #include this to not save the blank fields.
   # :reject_if => proc { |attributes| :all_blank, 
   
   field :name, type: String
